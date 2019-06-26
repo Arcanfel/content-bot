5a4b829e3ab599f7596c83889113302816d964b5
Artur Shinkevich <archican91@gmail.com>
2019-06-25 21:55:18 -0400

    remove unused stuff

--- Package.swift
+++ Package.swift
@@ -12,9 +12,7 @@ let package = Package(
         .package(url: "https://github.com/iwasrobbed/Down.git", from: "0.8.5"),
         .package(url: "https://github.com/realm/SwiftLint", from: "0.33.0"),
         .package(url: "https://github.com/nicklockwood/SwiftFormat", from: "0.40.10"),
-        .package(url: "https://github.com/orta/Komondor.git", from: "1.0.4"),
         .package(url: "https://github.com/vapor/vapor.git", from: "3.0.0"),
-        .package(url: "https://github.com/orta/PackageConfig.git", from: "0.10.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -29,20 +27,3 @@ let package = Package(
         ),
     ]
 )
-
-// The settings for the git hooks for our repo
-#if canImport(PackageConfig)
-    import PackageConfig
-    
-    let config = PackageConfiguration([
-        "komondor": [
-            // When someone has run `git commit`, first run
-            // SwiftFormat and the auto-correcter for SwiftLint
-            "pre-commit": [
-                "swift run swiftformat Sources",
-                "swift run swiftlint autocorrect --path Sources/",
-                "git add .",
-            ],
-        ],
-    ])
-#endif
